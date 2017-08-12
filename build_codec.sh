78f00f26e438260de1cf35c23e030dfefe0ce7aa
572682 <musavi_hamid@bah.com>
2017-08-12 16:12:46 -0400

    ODE-464 updated Dockerfile and makefile for ieee 1609.2

--- build_codec.sh
+++ build_codec.sh
@@ -20,13 +20,11 @@ export LD_LIBRARY_PATH=/usr/local/lib CC=gcc
 
 echo Generating SAE J2735 API ...
 mkdir -p /asn1_codec/sae_j2735_SEMI_api && cd /asn1_codec/sae_j2735_SEMI_api
-asn1c -gen-PER *.asn
-make -f Makefile.am.sample
+make
 
 echo Generating IEEE 1609.2 API
 mkdir -p /asn1_codec/ieee_1609dot2_api && cd /asn1_codec/ieee_1609dot2_api
-asn1c -gen-OER *.asn
-make -f Makefile.am.sample
+make
 
 #echo building asn1_codec ...
 #mkdir -p /asn1_codec/build && cd /asn1_codec/build
