27e3fb28ff4061fd604d43ed7a9890b654816290
572682 <musavi_hamid@bah.com>
2017-10-23 13:48:55 -0400

    ODE-587 updated run scripts

--- run-with-mount.cmd
+++ run-with-mount.cmd
@@ -1,1 +1,1 @@
-docker run -it -v %ASN1_CODEC_HOME%:/asn1_codec -e DOCKER_HOST_IP=%DOCKER_HOST_IP% asn1_codec:latest %*
+docker run -it -v %DOCKER_SHARED_VOLUME%:/asn1_codec -e DOCKER_HOST_IP=%DOCKER_HOST_IP% asn1_codec:latest %*
