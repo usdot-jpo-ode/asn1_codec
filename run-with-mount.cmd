f7f2c99c8e10c6dc34490c2a7f8d006064d451ab
572682 <musavi_hamid@bah.com>
2017-12-01 16:19:39 -0500

    Updated scripts

--- run-with-mount.cmd
+++ run-with-mount.cmd
@@ -1,1 +1,1 @@
-docker run -it -v %DOCKER_SHARED_VOLUME%:/asn1_codec -e DOCKER_HOST_IP=%DOCKER_HOST_IP% jpoode_acm:latest %*
+docker run -it -v %DOCKER_SHARED_VOLUME%:/asn1_codec_share -e DOCKER_HOST_IP=%DOCKER_HOST_IP% jpoode_acm:latest %*
--- run-with-mount.sh
+++ run-with-mount.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it -v ${DOCKER_SHARED_VOLUME}:/asn1_codec -e DOCKER_HOST_IP=${DOCKER_HOST_IP} asn1_codec:latest $*
+docker run -it -v ${DOCKER_SHARED_VOLUME}:/asn1_codec_share -e DOCKER_HOST_IP=${DOCKER_HOST_IP} asn1_codec:latest $*
