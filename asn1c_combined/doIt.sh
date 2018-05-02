b6d8e37942f746f37eaa2b78e6785413bf26575a
572682 <musavi_hamid@bah.com>
2018-05-02 17:06:58 -0400

    ODE-768 replaced scms-asn from CAMP to usdot-jpo-ode/scms-asn1

--- asn1c_combined/doIt.sh
+++ asn1c_combined/doIt.sh
@@ -4,8 +4,8 @@ export LD_LIBRARY_PATH=/usr/local/lib
 export CC=gcc
 
 asn1c -fcompound-names -gen-PER -gen-OER -pdu=all \
-    ../scms-asn/1609dot2-asn/1609dot2-base-types.asn \
-    ../scms-asn/1609dot2-asn/1609dot2-schema.asn \
+    ../scms-asn1/1609dot2-asn/1609dot2-base-types.asn \
+    ../scms-asn1/1609dot2-asn/1609dot2-schema.asn \
     J2735_201603DA.ASN \
     SEMI_v2.3.0_070616.asn \
     2>&1 | tee compile.out
