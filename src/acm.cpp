3d30f6621cc12da1810d77366ef55d0d8ccff411
Daniel Stephenson <dstephenson@trihydrol.com>
2022-02-10 11:08:35 -0700

    Removed some unnecessary passed config options.

--- src/acm.cpp
+++ src/acm.cpp
@@ -487,10 +487,6 @@ bool ASN1_Codec::configure() {
         conf->set("sasl.mechanism", "PLAIN", error_string);
         conf->set("sasl.username", username.c_str(), error_string);
         conf->set("sasl.password", password.c_str(), error_string);
-
-        // may or may not be necessary
-        conf->set("ssl.endpoint.identification.algorithm", "https", error_string);
-        conf->set("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required", error_string);
     }
     // end of confluent cloud integration
 
