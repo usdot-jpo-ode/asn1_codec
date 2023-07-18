672a7d25c00e289c33c4d27a1065cb0259560e36
Marc Wodahl <mwodahl@trihydro.com>
2023-07-18 12:47:23 -0600

    Update logging levels to uppercase strings

--- src/acm.cpp
+++ src/acm.cpp
@@ -430,19 +430,19 @@ bool ASN1_Codec::configure() {
     } 
     
     if (optIsSet('v')) {
-        if ("trace" == optString('v')) {
+        if ("TRACE" == optString('v')) {
             logger->set_level(spdlog::level::trace);
-        } else if ("debug" == optString('v')) {
+        } else if ("DEBUG" == optString('v')) {
             logger->set_level(spdlog::level::trace);
-        } else if ("info" == optString('v')) {
+        } else if ("INFO" == optString('v')) {
             logger->set_level(spdlog::level::trace);
-        } else if ("warning" == optString('v')) {
+        } else if ("WARNING" == optString('v')) {
             logger->set_level(spdlog::level::warn);
-        } else if ("error" == optString('v')) {
+        } else if ("ERROR" == optString('v')) {
             logger->set_level(spdlog::level::err);
-        } else if ("critical" == optString('v')) {
+        } else if ("CRITICAL" == optString('v')) {
             logger->set_level(spdlog::level::critical);
-        } else if ("off" == optString('v')) {
+        } else if ("OFF" == optString('v')) {
             logger->set_level(spdlog::level::off);
         } else {
             logger->warn("information logger level was configured but unreadable; using default.");
