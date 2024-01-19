e875f33629a1ffca705176f15b9c95df5cac6784
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2024-01-19 12:37:54 -0700

    Added generated implementation/header files to source control

--- asn1c_combined/doIt.sh
+++ asn1c_combined/doIt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script assumes that the J2735 files have already been generated.
+# To generate the J2735 files, run generate-files.sh with asn1c installed.
+
 export LD_LIBRARY_PATH=/usr/local/lib
 export CC=gcc
 
@@ -10,19 +13,16 @@ else
     year=$J2735_YEAR
 fi
 
-asn1c -fcompound-names -gen-OER -fincludes-quoted -no-gen-JER -pdu=all \
-    ../scms-asn1/1609dot2-asn/1609dot2-base-types.asn \
-    ../scms-asn1/1609dot2-asn/1609dot2-schema.asn \
-    ./j2735-asn-files/$year/*.asn \
-    ./SEMI_v2.4.0_121223_j2020.asn \
-    2>&1 | tee compile.out
+# Copy generated files to for specified year to asn1c_combined & extract
+echo "Extracting & copying generated files for $year"
+tar -xzf ./generated-files/$year.tar.gz
+cp ./generated-files/$year/* .
 
-# if 2020, copy overrides
-if [ "$year" == "2020" ]; then
-    echo "Copying overrides for $year"
-    cp ./j2735-asn-files/$year/overrides/*.h .
-fi
+# Compile example
+echo "Compiling example"
+sed -i 's/\(-DASN_PDU_COLLECTION\)/-DPDU=MessageFrame \1/' ./converter-example.mk
+make -f ./converter-example.mk
 
-sed -i 's/\(-DASN_PDU_COLLECTION\)/-DPDU=MessageFrame \1/' converter-example.mk
-
-make -f converter-example.mk
+# Clean up
+echo "Cleaning up"
+rm -rf ./generated-files/$year
